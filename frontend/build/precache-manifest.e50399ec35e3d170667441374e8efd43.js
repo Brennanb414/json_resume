89c3b78f1af18c6af16f93fef45b17c80adb8d8c
Brennan Brown <brennanb414@gmail.com>
2020-03-05 11:34:09 -0600

    add static build files

---
+++ frontend/build/precache-manifest.e50399ec35e3d170667441374e8efd43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b0e11bca30a7e83867451de66020ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eb2354912413eded8fb",
+    "url": "/static/css/main.af61a935.chunk.css"
+  },
+  {
+    "revision": "33a2e7ae729a9cfdd11c",
+    "url": "/static/js/2.5e55bb41.chunk.js"
+  },
+  {
+    "revision": "5eb2354912413eded8fb",
+    "url": "/static/js/main.061e715b.chunk.js"
+  },
+  {
+    "revision": "efc7ff6f2cdaed54b6f3",
+    "url": "/static/js/runtime-main.3570719c.js"
+  }
+]);
