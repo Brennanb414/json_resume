51035cb4fa6e10a1a903ab28b6c8693d70004794
Brennan Brown <brennanb414@gmail.com>
2020-03-06 13:17:30 -0600

    env variable tweaking & scripts setup

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ var express = require('express'),
   path = require('path');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://104.43.208.92/resumeDB');
+mongoose.connect(process.env.NODE_ENV === 'development' ? 'mongodb://104.43.208.92/resumeDB' : 'mongodb://localhost/resumeDB');
 
 const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', "*");
