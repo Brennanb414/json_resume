737e551baff5c6496d4d876be6aa39acec52ff71
Brennan Brown <brennanb414@gmail.com>
2020-03-16 13:51:01 -0500

    removes post/put capabilities for production

--- backend/api/routes/routes.js
+++ backend/api/routes/routes.js
@@ -5,14 +5,14 @@ module.exports = function(app) {
 
   app.route('/api/resume')
     .get(resume.list_all_resumes)
-    .post(resume.create_resume)
-    .put(resume.update_resume)
+    // .post(resume.create_resume)
+    // .put(resume.update_resume)
 
   app.route('/api/insects')
     .get(insects.list_all_insects)
-    .post(insects.update_insects)
-    .put(insects.update_insects)
-    .delete(insects.delete_insects)
+    // .post(insects.update_insects)
+    // .put(insects.update_insects)
+    // .delete(insects.delete_insects)
   app.route('/api/getFourInsects')
     .get(insects.list_four_insects)
 };
