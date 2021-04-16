b975734da18777f0338b2615c6129775666da7a0
Brennan Brown <brennanb414@gmail.com>
2021-04-16 16:09:59 -0500

    build files

---
+++ frontend/build/precache-manifest.de13492d7158604a06fcc2cacfe10d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6187d7ee57c80326868ae3831b7fcf2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1768a278133509a7f28d",
+    "url": "/static/css/main.8b69611c.chunk.css"
+  },
+  {
+    "revision": "ef267f30b6356b543278",
+    "url": "/static/js/2.e451b929.chunk.js"
+  },
+  {
+    "revision": "1768a278133509a7f28d",
+    "url": "/static/js/main.38c26b7a.chunk.js"
+  },
+  {
+    "revision": "ab53df34246b11bce97b",
+    "url": "/static/js/runtime-main.a7553ab0.js"
+  }
+]);
